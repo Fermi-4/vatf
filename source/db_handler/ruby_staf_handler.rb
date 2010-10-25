5c93fa5b9ac49b1885aab05691cf127c0dc1bd37
Elvita Lobo <elvita@ti.com>
2010-10-25 18:21:29 -0400

    Fix in ruby_staf_handler to check for mingw

--- source/db_handler/ruby_staf_handler.rb
+++ source/db_handler/ruby_staf_handler.rb
@@ -1,7 +1,7 @@
 if(system("staf local service help"))
   require File.dirname(__FILE__)+'/staf/STAFException'
   require File.dirname(__FILE__)+'/staf/STAFResult'
-  if RUBY_PLATFORM.downcase.include?("mswin")
+  if RUBY_PLATFORM.downcase.include?("mswin") || RUBY_PLATFORM.downcase.include?("mingw")
     require File.dirname(__FILE__)+'/staf/windows/STAFHandle'
   elsif RUBY_PLATFORM.downcase.include?("linux")
     require File.dirname(__FILE__)+'/staf/linux/STAFHandle'
