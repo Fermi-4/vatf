be90707ed9309a71513e131185c06f0a45a21364
Yan Liu <yan-liu@ti.com>
2011-11-01 09:36:52 -0400

    Avoid create a new instance if the object is already existing
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- source/target/base_driver.rb
+++ source/target/base_driver.rb
@@ -7,11 +7,15 @@ module Equipment
     def initialize(platform_info, log_path)
       @platform_info = platform_info
       @log_path      = log_path 
+      @iface = ''
       super(Object.new)
     end
     
     def set_api(iface_type)
-      self.__setobj__(Object.const_get(@api_list[iface_type]).new(@platform_info,@log_path))
+      if !__getobj__ || @iface != iface_type
+      	self.__setobj__(Object.const_get(@api_list[iface_type]).new(@platform_info,@log_path))
+        @iface=iface_type
+      end 
       rescue Exception => e
         raise e.backtrace.to_s+"\n Unable to start #{iface_type} interface with #{@platform_info.name} #{@platform_info.id}. Verify communication channel and settings"
     end
