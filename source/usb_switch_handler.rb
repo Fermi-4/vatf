01d7bcb213cbce74a6d179131b8d84ed9617d839
Felipe Balbi <balbi@ti.com>
2013-07-11 16:00:53 +0300

    usb_switch_handler: overal cleanup
    fix indentation, remove whitespaces.
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- source/usb_switch_handler.rb
+++ source/usb_switch_handler.rb
@@ -1,30 +1,27 @@
-
 class UsbSwitchHandler
 	attr_reader :usb_switch_controller
 	def initialize()
 		@usb_switch_controller = Hash.new
 	end
-	
+
 	def load_usb_ports(io_info)
-	  return if !io_info
+		return if !io_info
 		io_info.each_key do |key|
-				if !@usb_switch_controller[key.to_s.downcase]
-					@usb_switch_controller[key.to_s.downcase] = Object.const_get($equipment_table['usb_switch_controller'][key.to_s.downcase][0].driver_class_name).new($equipment_table['usb_switch_controller'][key.to_s.downcase][0])
-				end
+			if !@usb_switch_controller[key.to_s.downcase]
+				@usb_switch_controller[key.to_s.downcase] = Object.const_get($equipment_table['usb_switch_controller'][key.to_s.downcase][0].driver_class_name).new($equipment_table['usb_switch_controller'][key.to_s.downcase][0])
+			end
 		end
-		rescue Exception => e
-			raise e.to_s+"\nUnable to create power controller: " + io_info.to_s
+	rescue Exception => e
+		raise e.to_s+"\nUnable to create power controller: " + io_info.to_s
 	end
 
-	def disconnect(switch_name)
-		@usb_switch_controller[switch_name.to_s.downcase].disconnect()
+	def disconnect(switch)
+		@usb_switch_controller[switch.to_s.downcase].disconnect()
 	end
 
 	def select_input(input)
-    input.each {|switch_name,port|
-      @usb_switch_controller[switch_name.to_s.downcase].select_input(port)
-    }
+		input.each { |switch, port|
+			@usb_switch_controller[switch.to_s.downcase].select_input(port)
+		}
 	end
-
 end
-  
