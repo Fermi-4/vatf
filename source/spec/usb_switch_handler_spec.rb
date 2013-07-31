ba42ff09a2f4529bbb12d0ae47b2d28981b39eaf
Felipe Balbi <balbi@ti.com>
2013-07-31 21:42:59 +0300

    vatf: add usb_switch_handler specifications
    Add rspec specifications for usb_switch_handler.rb
    
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

---
+++ source/spec/usb_switch_handler_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+require 'rspec/mocks'
+require_relative File.join(File.expand_path(__FILE__), "..", "..", "usb_switch_handler")
+
+describe UsbSwitchHandler do
+  before :each do
+    @usb = UsbSwitchHandler.new
+  end
+
+  describe "#new"do
+    it "returns an instance of #{described_class}" do
+      expect(@usb).to be_an_instance_of described_class
+    end
+  end
+
+  describe "#usb_switch_controller" do
+    it { @usb.should respond_to(:usb_switch_controller) }
+    it { @usb.usb_switch_controller.should be_an_instance_of Hash }
+    it "raises error when trying to modify usb_switch_controller Hash" do
+      expect { @usb.usb_switch_controller = nil }.to raise_error
+    end
+  end
+
+  describe "#load_usb_ports" do
+    it { @usb.should respond_to(:load_usb_ports).with(1).arguments }
+
+    it "does not raise error with nil argument" do
+      expect { @usb.load_usb_ports(nil) }.to_not raise_error
+    end
+  end
+
+  describe "#disconnect" do
+    it { @usb.should respond_to(:disconnect).with(1).argument }
+  end
+
+  describe "#select_input" do
+    it { @usb.should respond_to(:select_input).with(1).argument }
+
+    it "doesn't raise and error" do
+      controller = double("usb_switch_controller", :select_input => 0)
+      input = double("input", :each => controller)
+
+      expect { @usb.select_input(input) }.to_not raise_error
+    end
+  end
+end
