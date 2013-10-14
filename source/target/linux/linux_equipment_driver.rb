2066d52a1e6b3932f192e2a8937e9a58cb0cb8ee
Carlos Hernandez <ceh@ti.com>
2013-10-14 10:09:09 -0400

    LinuxEquipmentDriver: Define default bootargs for dra7xx
    Signed-off-by: Carlos Hernandez <ceh@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- source/target/linux/linux_equipment_driver.rb
+++ source/target/linux/linux_equipment_driver.rb
@@ -44,6 +44,7 @@ module Equipment
      'beaglebone-black' => 'console=ttyO0,115200n8 ip=dhcp earlyprink ',
      'omap5-evm' => 'console=ttyO2,115200n8 ip=dhcp ',
      'tci6638-evm' => 'console=ttyS0,115200n8 rootwait=1 earlyprintk', 
+     'dra7xx-evm' => 'console=ttyO0,115200n8 ip=dhcp earlyprink ',
      })
     
     def initialize(platform_info, log_path)
