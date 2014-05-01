4187020ab5075f4e16ec3c47fc109e6401908c0c
Alejandro Hernandez <ajhernandez@ti.com>
2014-05-01 19:11:30 -0400

    Refactor power handler
    This patch refactors power handler to allow the use of arrays as
    parameter in functions switch_on, switch_off and get_status, as well
    as, allow the user to switch on, switch off and reset more than one
    port simultaneously.
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- source/power_handler.rb
+++ source/power_handler.rb
@@ -5,63 +5,72 @@ class PowerHandler
 		@power_controllers = Hash.new
 	end
 	
-        def load_power_ports(io_info)
-                return if !io_info
-                if (io_info.kind_of?(Array))
-                       io_info.each { |io_info_element|
-                                io_info_element.each_key do |key|
-                                        if !@power_controllers[key.to_s.downcase]
-                                           @power_controllers[key.to_s.downcase] = Object.const_get($equipment_table['power_controller'][key.to_s.downcase][0].driver_class_name).new($equipment_table['power_controller'][key.to_s.downcase][0])
-			               end
-                                end
-                                    }
-                else
-                    io_info.each_key do |key|
-                       if !@power_controllers[key.to_s.downcase]
-                                 @power_controllers[key.to_s.downcase] = Object.const_get($equipment_table['power_controller'][key.to_s.downcase][0].driver_class_name).new($equipment_table['power_controller'][key.to_s.downcase][0])
-                       end
-                    end
-                 end
-                 rescue Exception => e
-                 raise e.to_s+"\nUnable to create power controller: " + io_info.to_s
+  def load_power_ports(lio_info)
+    return if !lio_info
+    io_info = lio_info
+    io_info = [lio_info] if !io_info.kind_of?(Array)
+      io_info.each { |io_info_element|
+        io_info_element.each_key do |key|
+          if !@power_controllers[key.to_s.downcase]
+            @power_controllers[key.to_s.downcase] = Object.const_get($equipment_table['power_controller'][key.to_s.downcase][0].driver_class_name).new($equipment_table['power_controller'][key.to_s.downcase][0])
+          end
         end
+     }
+    rescue Exception => e
+      raise e.to_s+"\nUnable to create power controller: " + io_info.to_s
+  end
 
 	def disconnect
 		@power_controllers.each_value { |val| val.disconnect()}
 	end
 	
-	def get_status(power_port)
-		power_port.each {|key,val|
-			@power_controllers[key.to_s.downcase].get_status(val)
+	def get_status(p_port)
+	  power_port = p_port
+	  power_port = [p_port] if !p_port.kind_of?(Array)
+		power_port.each {
+		  port_info.each{|key,val|
+			  @power_controllers[key.to_s.downcase].get_status(val)
+			}
 		}
 	end
 	
-	def switch_on(power_port)
-		power_port.each {|key,val|
-			@power_controllers[key.to_s.downcase].switch_on(val)
+	def switch_on(p_port)
+	  power_port = p_port
+	  power_port = [p_port] if !p_port.kind_of?(Array)
+		power_port.each {
+		  port_info.each{|key,val|
+			  @power_controllers[key.to_s.downcase].switch_on(val)
+			}
 		}
 	end
 	
-	def switch_off(power_port)
-		power_port.each {|key,val|
-			@power_controllers[key.to_s.downcase].switch_off(val)
+	def switch_off(p_port)
+	  power_port = p_port
+	  power_port = [p_port] if !p_port.kind_of?(Array)
+		power_port.each {
+		  port_info.each{|key,val|
+			  @power_controllers[key.to_s.downcase].switch_off(val)
+		  }
 		}
 	end
 	
-	def reset(power_port)
-                if (power_port.kind_of?(Array))
-                  power_port.each {|power_port_element|
-		        power_port_element.each {|key,val|
-                             puts "ABOUT TO RESET #{power_port_element}\n"
-			     @power_controllers[key.to_s.downcase].reset(val)
-		                                }
-                                }
-                else
-                     power_port.each {|key,val|
-                             puts "ABOUT TO RESET #{power_port}\n"
-                             @power_controllers[key.to_s.downcase].reset(val)
-                                                }
-                end
-	end
+	def reset(p_port)
+	  power_port = p_port
+	  power_port = [p_port] if !p_port.kind_of?(Array)
+    power_port.each {|power_port_element|
+      power_port_element.each {|key,val|
+        puts "Turning off port #{val} at #{key}\n"
+	      @power_controllers[key.to_s.downcase].switch_off(val)
+      }
+    }
+    sleep 2
+    power_port.each {|power_port_element|
+      power_port_element.each {|key,val|
+        puts "Turning on port #{val} at #{key}\n"
+	      @power_controllers[key.to_s.downcase].switch_on(val)
+      }
+    }
+  end
+  
 end
   
