424fcc8944b9cf924f3092be5c8148d42e532583
Alejandro Hernandez <ajhernandez@ti.com>
2014-06-02 15:41:21 -0400

    Add TCP/IP connection listener
    This patch adds a TcpIpBaseListenerClient class in order to
    support TCP/IP connections
    
    Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

--- source/target/base_listener.rb
+++ source/target/base_listener.rb
@@ -1,5 +1,6 @@
 require 'serialport'
 require 'thread'
+require 'socket'
 
 class BaseListener 
   attr_reader :match, :cmd, :expect, :response
@@ -11,11 +12,11 @@ class BaseListener
       i+=1
     }
     @cmd = Regexp.new(Regexp.escape(first_cmd_word))
-	  @expect = /#{expect}/m
-	  @check_cmd_echo = check_cmd_echo
+    @expect = /#{expect}/m
+    @check_cmd_echo = check_cmd_echo
     @response = ''
     @match = false
-	end
+  end
 
   def process_response(data)
     @response += data
@@ -23,7 +24,7 @@ class BaseListener
   
   def match
     @match = (@response.index(@expect) && (!@check_cmd_echo || @response.index(/#{@cmd}.+?#{@expect}/m)) )
-	  @match
+    @match
   end
 end
   
@@ -72,7 +73,7 @@ class SerialBaseListenerClient < SerialPort
           Kernel.print last_read
        end
       end
-    }	
+    }  
   end
   
   def stop_listening
@@ -105,21 +106,21 @@ class TelnetBaseListenerClient < Net::Telnet
   
   def initialize(platform_info,serial_server)
     if serial_server
-	    @telnet_ip   = platform_info.serial_server_ip
-	    @telnet_port = platform_info.serial_server_port
-	  else
-	    @telnet_ip   = platform_info.telnet_ip
+      @telnet_ip   = platform_info.serial_server_ip
+      @telnet_port = platform_info.serial_server_port
+    else
+      @telnet_ip   = platform_info.telnet_ip
       @telnet_port = platform_info.telnet_port
-	  end
-	@prompt      = platform_info.prompt
+    end
+  @prompt      = platform_info.prompt
   @boot_prompt = platform_info.boot_prompt
   @telnet_login = platform_info.telnet_login
   @telnet_passwd = platform_info.telnet_passwd
-	@login_prompt  = platform_info.login_prompt if platform_info.respond_to?:login_prompt
-	@password_prompt  = platform_info.password_prompt if platform_info.respond_to?:password_prompt
-	@timeout = platform_info.timeout if platform_info.respond_to?:timeout
-	platform_info.telnet_bin_mode != nil ? @telnet_bin_mode = platform_info.telnet_bin_mode : @telnet_bin_mode = true
-	super( "Host" => @telnet_ip,
+  @login_prompt  = platform_info.login_prompt if platform_info.respond_to?:login_prompt
+  @password_prompt  = platform_info.password_prompt if platform_info.respond_to?:password_prompt
+  @timeout = platform_info.timeout if platform_info.respond_to?:timeout
+  platform_info.telnet_bin_mode != nil ? @telnet_bin_mode = platform_info.telnet_bin_mode : @telnet_bin_mode = true
+  super( "Host" => @telnet_ip,
            "Port" => @telnet_port,
            "Waittime" => 0,
            "Prompt" => @prompt,
@@ -155,7 +156,7 @@ class TelnetBaseListenerClient < Net::Telnet
           Kernel.print last_read
         end
       end
-    }	
+    }  
   end
   
   def stop_listening
@@ -186,5 +187,69 @@ class TelnetBaseListenerClient < Net::Telnet
       super
     end
   end
+end
 
+class TcpIpBaseListenerClient < TCPSocket
+  include Log4r  
+  attr_reader :response
+  
+  def initialize(platform_info)
+    @telnet_ip   = platform_info.telnet_ip
+    @telnet_port = platform_info.telnet_port
+    super( @telnet_ip,
+           @telnet_port)
+    binmode() if platform_info.telnet_bin_mode
+  end
+  
+  def add_listener(listener)
+    @listeners << listener
+  end
+  
+  def remove_listener(listener)
+    @listeners.delete(listener)
+  end
+  
+  def notify_listeners(data)
+    @listeners.each { |listener|
+      listener.process_response(data)
+    }
+    @session_data += data
+  end
+  
+  def start_listening
+    @listeners = Array.new
+    @session_data = ''
+    @keep_listening = true
+    @listen_thread = Thread.new {
+      while @keep_listening
+        if !eof?
+          #last_read = readpartial(1024) 
+          last_read = read_nonblock(1024) 
+          notify_listeners(last_read)
+          Kernel.print last_read
+        end
+      end
+    }  
+  end
+  
+  def stop_listening
+    @keep_listening = false
+    if @listen_thread
+      @listen_thread.kill 
+      @listen_thread.join(5)
+    end
+  end
+
+  def send_cmd(command)
+    puts(command)
+  end
+  
+  def write_cmd(command)
+    write(command)
+  end
+
+  def disconnect
+    stop_listening
+    self.close
+  end
 end
