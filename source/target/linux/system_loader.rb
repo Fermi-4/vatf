a55726f4b88c3753f423508b5525ee3ef9e1bba8
Carlos Hernandez <ceh@ti.com>
2014-09-10 15:25:27 -0400

    Relax response check on login
    Checking for login name followed by system prompt causes problems
    sometimes as the data comming from the DUT may be garbled, so
    relax the check by only expecting system prompt as that is
    sufficient proof that the board logged in.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>
    Signed-off-by: Yan Liu <yan-liu@ti.com>

--- source/target/linux/system_loader.rb
+++ source/target/linux/system_loader.rb
@@ -547,7 +547,7 @@ module SystemLoader
     def run(params)
       send_cmd params, "boot", params['dut'].login_prompt, 150
       params['dut'].boot_log = params['dut'].response
-      send_cmd params, params['dut'].login, params['dut'].prompt, 10 # login to the unit
+      send_cmd params, params['dut'].login, params['dut'].prompt, 10, false # login to the unit
     end
   end
 
