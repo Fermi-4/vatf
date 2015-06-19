2dada5e4a26dd89b26c448c36f499f0493d111dd
Carlos Hernandez <ceh@ti.com>
2015-06-19 17:39:24 -0400

    cmd_translator: Update en/dis USB commands for am43x
    The sysfs path to enable/disable USB wakeup changed on 3.14.43 so
    update the same in cmd_translator.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- source/lib/cmd_translator.rb
+++ source/lib/cmd_translator.rb
@@ -66,6 +66,9 @@ module CmdTranslator
                     '3.14' => Hash.new('').merge!(
                           {'am335x-evm' => 'lst=`find /sys/devices/ocp/47400000.usb/ -name wakeup`; for ent in $lst; do echo $ent; echo enabled > $ent; done',
                            'am43xx-gpevm' => 'lst=`find /sys/devices/44000000.ocp/483c0000.omap_dwc3/ -name wakeup`; for ent in $lst; do echo $ent; echo enabled > $ent; done',} ),
+                    '3.14.43' => Hash.new('').merge!(
+                          {'am43xx-epos' => 'lst=`find /sys/devices/44000000.ocp/*ocp2scp*/ -name wakeup`; for ent in $lst; do echo $ent; echo enabled > $ent; done',
+                           'am43xx-gpevm' => 'lst=`find /sys/devices/44000000.ocp/*ocp2scp*/ -name wakeup`; for ent in $lst; do echo $ent; echo enabled > $ent; done',} ),
     },
     'disable_usb_wakeup' => { 
                     '0.0' => Hash.new('').merge!(
@@ -76,6 +79,9 @@ module CmdTranslator
                           {'am335x-evm' => 'lst=`find /sys/devices/ocp/47400000.usb/ -name wakeup`; for ent in $lst; do echo $ent; echo disabled > $ent; done',
                            'am43xx-epos' => 'lst=`find /sys/devices/44000000.ocp/ocp2scp.* -name wakeup`; for ent in $lst; do echo $ent; echo disabled > $ent; done',
                            'am43xx-gpevm' => 'lst=`find /sys/devices/44000000.ocp/ocp2scp.* -name wakeup`; for ent in $lst; do echo $ent; echo disabled > $ent; done',} ),
+                    '3.14.43' => Hash.new('').merge!(
+                          {'am43xx-epos' => 'lst=`find /sys/devices/44000000.ocp/*ocp2scp*/ -name wakeup`; for ent in $lst; do echo $ent; echo disabled > $ent; done',
+                           'am43xx-gpevm' => 'lst=`find /sys/devices/44000000.ocp/*ocp2scp*/ -name wakeup`; for ent in $lst; do echo $ent; echo disabled > $ent; done',} ),
     },
     'disable_tsc_wakeup' => { 
                     '0.0' => Hash.new('').merge!(
