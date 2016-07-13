dfd7ff50149911b02c0e01040521aa50360c6eb7
Carlos Hernandez <ceh@ti.com>
2016-07-13 14:55:08 -0400

    Linux:SystemLoader: Increase dhcp timeout
    10 seconds is too aggresive to declare dhcp is not working. We have
    seen cases where dhcp took more than 10 seconds.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- source/target/linux/system_loader.rb
+++ source/target/linux/system_loader.rb
@@ -427,7 +427,7 @@ module SystemLoader
         send_cmd params, "setenv autoload 'no'", nil, 2, false, false
         3.times do |trial|
           begin
-            send_cmd params, "dhcp", /DHCP client bound to address.*#{params['dut'].boot_prompt}/im, 10
+            send_cmd params, "dhcp", /DHCP client bound to address.*#{params['dut'].boot_prompt}/im, 20
             break
           rescue Exception => e
             params['dut'].send_abort()
