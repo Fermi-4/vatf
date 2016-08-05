fe754e1fc4e193e589ddcff36cf17902a416bda7
Carlos Hernandez <ceh@ti.com>
2016-08-05 17:07:25 -0400

    STAFResult: Handle marshalled and unmarshalled data.
    Some services in Opentest return unmarshalled data (e.g. new RESMGR
    requests provided by DISPATCHER). Previous unmarshall_reponse method
    assumed that data was always marshalled. Modify the method to handle
    both types of data.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- source/db_handler/staf/STAFResult.rb
+++ source/db_handler/staf/STAFResult.rb
@@ -12,11 +12,20 @@ module STAF
       values = []
       map = {}
       #puts "\nSTAF RESPONSE:\n #{response} \n"
-      response.scan(/\{:\d+::12:display-name@SDT.+?key@SDT\/\$S:\d+:(\w+?)(?::[:\w]+)*@SDT/m) {|key| keys << key.flatten[0]}
-      raw_values = response.scan(/%:\d+::\d+:.+?@SDT(.+)$/m).flatten[0].split(/@SDT/)
-      #raw_values.each {|key| puts "raw_values=#{key}"}
-      raw_values.each {|val| values << /\/\$S:\d+:(.*)/m.match(val).captures[0]}
-      keys.each_index {|i| map[keys[i]] = values[i]}
+      begin
+        response.scan(/\{:\d+::12:display-name@SDT.+?key@SDT\/\$S:\d+:(\w+?)(?::[:\w]+)*@SDT/m) {|key| keys << key.flatten[0]}
+        raw_values = response.scan(/%:\d+::\d+:.+?@SDT(.+)$/m).flatten[0].split(/@SDT/)
+        #raw_values.each {|key| puts "raw_values=#{key}"}
+        raw_values.each {|val| values << /\/\$S:\d+:(.*)/m.match(val).captures[0]}
+        keys.each_index {|i| map[keys[i]] = values[i]}
+      rescue
+      end
+      if map.empty?
+        map = {}
+        #puts "\nSTAF RESPONSE:\n #{response} \n"
+        d=response.sub(/^@SDT\/\{:\d+:/,'')
+        d.scan(/:\d+?:(.+?)@SDT\/\$S:\d+:([^:]*)/) {|key, value| map[key]=value}
+      end
       map
     end
   end
