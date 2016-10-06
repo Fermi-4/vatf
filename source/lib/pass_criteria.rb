2371e8eb97e03e2506e7a174376a269e0b6de366
Carlos Hernandez <ceh@ti.com>
2016-10-06 10:21:13 -0400

    PassCriteria: Bypass proxy on first connection to server
    Try to connect to Metrics server without proxy first, if connection
    fails then go thru proxy.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- source/lib/pass_criteria.rb
+++ source/lib/pass_criteria.rb
@@ -58,7 +58,11 @@ module PassCriteria
     puts "testplan:#{testplan_id}, testcase:#{testcase_id}, metric:#{metric_name}, op:#{operator}"
     host, port = SiteInfo::ANALYTICS_SERVER.split(':')
     port = port ? port.to_i : 3000      # ANALYTICS_SERVER runs on port 3000 by default
-    response = Net::HTTP.get(host, "/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/", port)
+    connection = Net::HTTP.new(host, port, nil)
+    response = connection.get("/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/")
+    if response.code != "200"
+      response = Net::HTTP.get(host, "/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/", port)
+    end
     s0 = response.match(/samples=([\-\d\.]+)/).captures[0].to_f
     data = []
     if s0 > 1
