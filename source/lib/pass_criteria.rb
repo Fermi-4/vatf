Fermi-4/vatf

commit 1610c1ee909a4aa64a7f7aad1a2baba9e6c22f01

PassCriteria: Handle different return types for Http Get
(Net::HTTP).get != NET::HTTP::get
Left side returns Response object while right side returns string.

Signed-off-by: Carlos Hernandez <ceh@ti.com>

source/lib/pass_criteria.rb
@@ -59,7 +59,7 @@ module PassCriteria
     host, port = SiteInfo::ANALYTICS_SERVER.split(':')
     port = port ? port.to_i : 3000      # ANALYTICS_SERVER runs on port 3000 by default
     connection = Net::HTTP.new(host, port, nil)
-    response = connection.get("/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/")
+    response = connection.get("/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/").body
     if response.code != "200"
       response = Net::HTTP.get(host, "/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/", port)
     end

commit 9a4a617e3acaa35c7cdff2619c658c4830765ed2

Fix bug introduced by 1610c1ee909a4a
Signed-off-by: Carlos Hernandez <ceh@ti.com>

source/lib/pass_criteria.rb
@@ -59,8 +59,9 @@ module PassCriteria
     host, port = SiteInfo::ANALYTICS_SERVER.split(':')
     port = port ? port.to_i : 3000      # ANALYTICS_SERVER runs on port 3000 by default
     connection = Net::HTTP.new(host, port, nil)
-    response = connection.get("/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/").body
-    if response.code != "200"
+    resp = connection.get("/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/")
+    response = resp.body
+    if resp.code != "200"
       response = Net::HTTP.get(host, "/performance/passcriteria/#{testplan_id}/#{testcase_id}/#{metric_name}/", port)
     end
     s0 = response.match(/samples=([\-\d\.]+)/).captures[0].to_f