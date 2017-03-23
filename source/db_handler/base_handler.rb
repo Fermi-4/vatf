Fermi-4/vatf

commit 08c47e76733100b2d533a45067d24e670f918b76

Make @test_params.var_boot_attempts writable
There are some cleanup cases where a minimum number of boot attempts
is desirable, so allow scripts to overwrite the default value.

Signed-off-by: Carlos Hernandez <ceh@ti.com>

source/db_handler/base_handler.rb
@@ -92,7 +92,7 @@ module ATFDBHandlers
         attr_reader :params_chan, :params_equip, :params_control
         attr_reader *tcase_attr.keys
         attr_reader *additional_parameters.keys
-        attr_accessor :image_path, :platform, :target
+        attr_accessor :image_path, :platform, :target, :var_boot_attempts
         define_method(:initialize) do
           @params_chan = ParamsChan.new()  
           @params_equip = ParamsEquip.new()

commit 330eb3b03470454bb42abd44505cd55b77a591eb

base_handler: Fix issue introduced by commit 08c47e76733
Patch "Make @test_params.var_boot_attempts writable"
declared var_boot_attempts as attr_accessor statically.
This has the unintended consequence of always making the
variable 0 and disregarding the value passed via staf.
This patch removes the static declaration and creates
the variable at runtime. The patch also cleans up the
implementation of other methods in the class

Signed-off-by: Alejandro Hernandez <ajhernandez@ti.com>

source/db_handler/base_handler.rb
@@ -92,7 +92,7 @@ module ATFDBHandlers
         attr_reader :params_chan, :params_equip, :params_control
         attr_reader *tcase_attr.keys
         attr_reader *additional_parameters.keys
-        attr_accessor :image_path, :platform, :target, :var_boot_attempts
+        attr_accessor :image_path, :platform, :target
         define_method(:initialize) do
           @params_chan = ParamsChan.new()  
           @params_equip = ParamsEquip.new()
@@ -117,45 +117,39 @@ module ATFDBHandlers
           end
         end
 	          
-        def method_missing(sym, *args, &block)
-          if @staf_handle
-            staf_req = @staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/sw_assets/#{sym}")
-            staf_req2 = @staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/tee/#{sym}")
-            if (staf_req.rc == 0)
-              staf_req.result
-            elsif(staf_req2.rc == 0)
-              staf_req2.result
-            else 
-              raise UndefinedSwAsset.new("Undefined sw asset named #{sym}")
-            end
-          else
-            super
+      def method_missing(sym, *args, &block)
+        return send(sym) if respond_to?(sym)
+        if @staf_handle
+          raise UndefinedSwAsset.new("Undefined sw asset named #{sym}")
+        end
+        super
+      end
+		
+      def instance_variable_defined?(sym)
+        super(sym) || create_w_staf(sym)
+      end
+      
+      def instance_variable_get(sym)
+        super(sym) if instance_variable_defined?(sym)
+      end
+        
+      def respond_to?(method_sym, include_private = false)
+        super(method_sym, include_private) || create_w_staf(method_sym)
+      end
+          
+      def create_w_staf(sym)
+        if @staf_handle
+          symbol_prep = sym.to_s.gsub(/^[:@]+/,'')
+          instance_result = (@staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/sw_assets/#{symbol_prep}")) 
+          instance_result = (@staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/tee/#{symbol_prep}")) if instance_result.rc != 0
+          if instance_result.rc == 0
+            self.class.send(:attr_accessor, symbol_prep)
+            instance_variable_set("@#{symbol_prep}",instance_result.result)
+            return instance_result.result
           end
         end
-		
-		def instance_variable_defined?(sym)
-		  result = super(sym)
-          result = result || (@staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/sw_assets/#{sym.to_s.gsub(/^[:@]+/,'')}")).rc == 0 if @staf_handle
-		  result = result || (@staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/tee/#{sym.to_s.gsub(/^[:@]+/,'')}")).rc == 0 if @staf_handle
-          result
-		end
-    
-    def instance_variable_get(sym)
-		  result = super(sym)
-      if (!result && @staf_handle)
-        instance_result = (@staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/sw_assets/#{sym.to_s.gsub(/^[:@]+/,'')}")) 
-        instance_result = (@staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/tee/#{sym.to_s.gsub(/^[:@]+/,'')}")) if instance_result.rc != 0
-        result = instance_result.result if instance_result.rc == 0
+        nil
       end
-      result
-		end
-        
-        def respond_to?(method_sym, include_private = false)
-          result = super(method_sym, include_private)
-          result = result || (@staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/sw_assets/#{method_sym}")).rc == 0 if @staf_handle
-		  result = result || (@staf_handle.submit("local","VAR","GET SHARED VAR #{@staf_service_name ? @staf_service_name+'/' : ''}auto/tee/#{method_sym}")).rc == 0 if @staf_handle
-          result
-        end
         
       end
       TestParameters.new() 