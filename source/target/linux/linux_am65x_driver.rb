c877cb7c71414f29460eb06fc2ef911bae3de3da
Carlos Hernandez <ceh@ti.com>
2018-07-31 19:23:40 -0400

    Define LinuxArm64Driver
    Generic driver to use with ARM64-based machines.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- source/target/linux/linux_am65x_driver.rb
+++ source/target/linux/linux_am65x_driver.rb
@@ -8,52 +8,27 @@ require File.dirname(__FILE__)+'/system_loader'
 
 module Equipment
 
-  class LinuxAm65xDriver < LinuxEquipmentDriver
+  class LinuxArm64Driver < LinuxEquipmentDriver
 
-    class R5BootStep < SystemLoader::UbootStep
+    class Arm64BootCmdStep < SystemLoader::UbootStep
       def initialize
-        super('r5_boot')
+        super('arm64_boot_cmd')
       end
 
       def run(params)
-        boot_timeout = params['var_boot_timeout'] ? params['var_boot_timeout'].to_i : 210
-        send_cmd params, "rproc init; rproc list;rproc load 1 0x70000000 0x200; rproc start 1", /.*/, 1, true, false
-        params['dut'].target.default = params['dut'].target.serial
-        send_cmd params, "", params['dut'].login_prompt, boot_timeout, true, false
-        params['dut'].boot_log = params['dut'].response
-        params['dut'].check_for_boot_errors()
-        if params['dut'].timeout?
-          params['dut'].log_info("Collecting kernel traces via sysrq...")
-          params['dut'].send_sysrq('t')
-          params['dut'].send_sysrq('l')
-          params['dut'].send_sysrq('w')
-        end
-        3.times {
-          send_cmd params, params['dut'].login, params['dut'].prompt, 40, false, false # login to the unit
-          break if !params['dut'].timeout?
-        }
-        raise "Error executing boot" if params['dut'].timeout?
-
-      end
-    end
-
-    class SwitchConsoleStep < SystemLoader::UbootStep
-      def initialize
-        super('switch_console')
-      end
-
-      def run(params)
-        params['dut'].target.default = params['dut'].target.bootloader
+        ramdisk_addr = params['_env']['initramfs']
+        dtb_addr     = ''
+        dtb_addr = params['_env']['dtb_loadaddr'] if params['dtb_image_name'].strip != ''
+        append_text params, 'bootcmd', "if iminfo #{params['_env']['kernel_loadaddr']}; then bootm #{params['_env']['kernel_loadaddr']} #{ramdisk_addr} #{dtb_addr};"\
+                                       " else booti #{params['_env']['kernel_loadaddr']} #{ramdisk_addr} #{dtb_addr}; fi"
       end
     end
 
 
     # Select SystemLoader's Steps implementations based on params
     def set_systemloader(params)
-      @system_loader = SystemLoader::AtfSystemLoader.new
-      @system_loader.remove_step('')
-      @system_loader.replace_step('boot', R5BootStep.new)  if @system_loader.contains?('boot')
-      @system_loader.insert_step_before('setip', SwitchConsoleStep.new) if @system_loader.contains?('setip')
+      @system_loader = SystemLoader::UbootSystemLoader.new
+      @system_loader.replace_step('boot_cmd', Arm64BootCmdStep.new)  if @system_loader.contains?('boot')
     end
 
   end
