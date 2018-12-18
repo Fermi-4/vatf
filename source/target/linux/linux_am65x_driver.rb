707eae7b4ff37294fd875edc3fd72f09db6ab91f
Carlos Hernandez <ceh@ti.com>
2018-12-18 17:06:32 -0500

    LinuxArm64Driver: Move Arm64BootCmdStep
    Move Arm64BootCmdStep to generic SystemLoader module to make it
    available to any SystemLoader class.
    
    Signed-off-by: Carlos Hernandez <ceh@ti.com>

--- source/target/linux/linux_am65x_driver.rb
+++ source/target/linux/linux_am65x_driver.rb
@@ -63,25 +63,10 @@ module Equipment
       end
     end
 
-    class Arm64BootCmdStep < SystemLoader::UbootStep
-      def initialize
-        super('arm64_boot_cmd')
-      end
-
-      def run(params)
-        ramdisk_addr = params['_env']['initramfs']
-        dtb_addr     = ''
-        dtb_addr = params['_env']['dtb_loadaddr'] if params['dtb_image_name'].strip != ''
-        append_text params, 'bootcmd', "if iminfo #{params['_env']['kernel_loadaddr']}; then bootm #{params['_env']['kernel_loadaddr']} #{ramdisk_addr} #{dtb_addr};"\
-                                       " else booti #{params['_env']['kernel_loadaddr']} #{ramdisk_addr} #{dtb_addr}; fi"
-      end
-    end
-
-
     # Select SystemLoader's Steps implementations based on params
     def set_systemloader(params)
       super(params)
-      @system_loader.replace_step('boot_cmd', Arm64BootCmdStep.new)  if @system_loader.contains?('boot')
+      @system_loader.replace_step('boot_cmd', SystemLoader::Arm64BootCmdStep.new)  if @system_loader.contains?('boot')
     end
 
   end
