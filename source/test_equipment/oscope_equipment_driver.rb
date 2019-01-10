a33da057ff019d6fcb5479515f5d6b05a53ed992
Vrinda Khetan <vrinda@ti.com>
2019-01-10 12:40:13 -0500

    Oscilloscope driver enhancements - screen-shot and duration based testing
    PTEST-1027: PLSDK: Enable Oscilloscope Automation for PTP OC Jitter testing
    
    Enhanced oscilloscope driver with two features:
    1. Download oscilloscope screen-shot
    2. Take measurement for given time (instead of number of measurements)
    
    Signed-off-by: Vrinda Khetan <vrinda@ti.com>
    Signed-off-by: Elvita Lobo <elvita@ti.com>

--- source/test_equipment/oscope_equipment_driver.rb
+++ source/test_equipment/oscope_equipment_driver.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__)+'/../target/equipment_driver'
 require 'socket'
 require "net/http"
 require "uri"
+require "open-uri"
 
 module TestEquipment
 
@@ -22,6 +23,19 @@ module TestEquipment
       log_info = $1
       puts log_info+"\n\n"
       return log_info
+    end
+
+    # This function enables oscillosope screenshot download over http
+    def get_oscope_screenshot()
+      time = Time.now.to_i  # or format however you like
+      oscope_screenshot_file = "oscope_screenshot_#{@telnet_ip}_#{time}.png"
+      Net::HTTP.start("#{@telnet_ip}") {
+        |http| resp = http.get("/image.png")
+        open("#{oscope_screenshot_file}" ,"wb") {
+          |file| file.write(resp.body)
+        }
+      }
+      return oscope_screenshot_file
     end
 
     # This function compares the oscilloscope version with that defined in bench file.
@@ -56,11 +70,12 @@ module TestEquipment
     # Assumption here is that channel-a is the master (or reference) while channel-b is the slave.
     # Input Parameters => input channels and number of times the measurement is to be made.
     # Return value(s) => measured jitter value(s).
-    def measure_jitter(cha=1, chb=2, loop_count=5)
+    def measure_jitter(cha=1, chb=2, timeout=20)
       sleep 5
       counter = 0
+      jitter_measurement = Array.new {Array.new}
       jitter_reading = Array.new
-
+      time_reading = Array.new
       # Make sure oscope is configured and DUT is in the right state
       sleep 5
 
@@ -78,8 +93,16 @@ module TestEquipment
       send_cmd("SELECT:CH#{cha} ON")
       send_cmd("SELECT:CH#{chb} ON")
       send_cmd("HORIZONTAL:MAIN:SCALE 20E-9")
+      send_cmd("HORIZONTAL:RECORDLENGTH 500")
       send_cmd("CH#{cha}:SCALE 2")
       send_cmd("CH#{chb}:SCALE 2")
+      send_cmd("ACQUIRE:MODE SAMPLE")
+      send_cmd("ACQUIRE:STOPAFTER RUNSTop")
+      send_cmd("REM 'Enable the status registers'")
+      send_cmd("DESE 1")
+      send_cmd("*ESE 1")
+      send_cmd("*SRE 32")
+      #send_cmd("ACQUIRE:STOPAFTER SEQuence")
       send_cmd("ACQUIRE:STATE RUN")
       # Sets the trigger level to 2.0V
       send_cmd("TRIGGER:A:LEVEL 2.0")
@@ -107,60 +130,85 @@ module TestEquipment
       send_cmd("EDGE2 FALL;")
       # turn the measurement on
       send_cmd(":MEASU:MEAS4:STATE ON;")
-
-      # Jitter Measurement
-      while counter < loop_count
-        # Wait until acquisition is complete before taking measurement
-        send_cmd("While BUSY? keep looping")
+      # Jitter Measurement for timeout seconds.
+      # Measure start time
+      last_tick = Time.now
+      while (Time.now - last_tick <= timeout)
+        # Wait until the acquisition is complete before taking the measurement
+        send_cmd("*OPC?")
+        # Take measurement on acquired data
         jitter_reading[counter] = send_cmd("MEASUREMENT:MEAS4:VALUE?").to_f
+        time_reading[counter] = Time.now.to_s
+        puts time_reading[counter]
         counter += 1
       end
-      puts "Jitter measurement(s) between reference channel #{cha} and slave channel #{chb} [in s]:"
-       jitter_reading.each{|elem| puts "#{elem}"}
-     return jitter_reading
+      # Combine jitter reading and timestamp arrays into a single two dimentsional array, jitter_measurement
+      jitter_measurement = jitter_reading. zip(time_reading)
+      jitter_measurement.each_with_index {|elem, index| puts "Jitter measurement [in s]: #{elem[0]} at #{elem[1]}"}
+      oscope_screenshot_file = get_oscope_screenshot()
+      return jitter_measurement, oscope_screenshot_file
     end
 
     # This function measures the time period of the signal connected to specified channel.
     # Input Parameters => input channel and number of times the measurement is to be made.
     # Return value(s) => measured time period value(s).
-    def measure_time_period(ch=1, loop_count=5)
+    def measure_time_period(ch=1, timeout=5)
+      period_measurement = Array.new {Array.new}
+      time_reading = Array.new
       period_reading = Array.new
       counter = 0
       # Make sure oscope is configured and DUT is in the right state
       sleep 5
-
       max_channels = @params['number_of_channels']
       if (ch > max_channels)
         puts "ERROR: Specified channel number is not supported. This oscilloscope supports up to #{max_channels}."
         return 0
       end
-
       # Setup time period measurement.
       # A horizontal unit of 1s was chosen since typical signal measurement
       # is for 1Hz or 1PPS (pulse per second) or signal time period = 1s.
       # A vertical unit of 2V was chosen since typical Sitara output voltage is ~2V.
+      # Turn all channels off first
+      send_cmd("SELECT:CH1 OFF")
+      send_cmd("SELECT:CH2 OFF")
+      send_cmd("SELECT:CH3 OFF")
+      send_cmd("SELECT:CH4 OFF")
+      # Turn on selected o-scope channel
       send_cmd("SELECT:CH#{ch} ON")
       send_cmd("HORIZONTAL:MAIN:SCALE 1")
       send_cmd("CH#{ch}:SCALE 2")
       # Acquire waveform
       send_cmd("ACQUIRE:STOPAFTER RUNSTop")
+      send_cmd("REM 'Enable the status registers'")
+      send_cmd("DESE 1")
+      send_cmd("*ESE 1")
+      send_cmd("*SRE 32")
       send_cmd("ACQUIRE:STATE RUN")
       send_cmd("SELect:CH#{ch}")
       send_cmd("MEASUREMENT:MEAS1:SOURCE CH#{ch}")
       send_cmd("MEASUREMENT:MEAS1:TYPE PERIod")
       # Turn on the waveform display on oscilloscope
       send_cmd("MEASUREMENT:MEAS1:STATE ON")
-
-      # Measure loop_count times, with a specified error margin.
-       while counter < loop_count
-        # Wait until acquisition is complete before taking measurement
-        send_cmd("While BUSY? keep looping")
+      # Oscilloscope settling time
+      sleep (2)
+      # Measure period for timeout seconds.
+      # Measure start time
+      last_tick = Time.now
+      while (Time.now - last_tick <= timeout)
+        # Wait until the acquisition is complete before taking the measurement
+        send_cmd("*OPC?")
+        send_cmd("Wait for read from Output Queue.")
+        #Take measurement on acquired data
         period_reading[counter] = send_cmd("MEASUREMENT:MEAS1:VALUE?").to_f
+        time_reading[counter] = Time.now.to_s
+        puts time_reading[counter]
         counter += 1
-       end
-       puts "Time period measurement(s) on channel #{ch} [in s]:"
-       period_reading.each{|elem| puts "#{elem}"}
-     return period_reading
+      end
+      # Combine period and timestamp arrays into a single two dimentsional array, period_measurement
+      period_measurement = period_reading. zip(time_reading)
+      period_measurement.each_with_index {|elem, index| puts "Time period measurement(s) on channel #{ch} [in s]: #{elem[0]} at #{elem[1]}"}
+      oscope_screenshot_file = get_oscope_screenshot()
+      return period_measurement, oscope_screenshot_file
     end
   end
 end
